ce35fd46628d3f5c2aa164fc86483ef4545d210c
Uditha98L <uditha98@yahoo.com>
2026-07-17 17:36:16 -0400

    Implement main plugin class and hooks

--- SRC/Plugin.php
+++ SRC/Plugin.php
@@ -1,1 +1,58 @@
+<?php
+/**
+ * Main plugin class.
+ *
+ * @package KoombiyoShipping
+ */
 
+namespace Koombiyo\Shipping;
+
+defined( 'ABSPATH' ) || exit;
+
+
+/**
+ * Class Plugin
+ */
+class Plugin {
+
+
+	/**
+	 * Register WordPress hooks.
+	 *
+	 * @return void
+	 */
+	public function register_hooks(): void {
+
+
+		add_action(
+			'init',
+			array(
+				$this,
+				'load_textdomain',
+			)
+		);
+
+
+	}
+
+
+	/**
+	 * Load translations.
+	 *
+	 * @return void
+	 */
+	public function load_textdomain(): void {
+
+		load_plugin_textdomain(
+			'koombiyo-shipping',
+			false,
+			dirname(
+				plugin_basename(
+					KOOMBIYO_SHIPPING_FILE
+				)
+			) . '/languages'
+		);
+
+	}
+
+}
