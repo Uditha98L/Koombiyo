a14ffc08ca2642de65038a93b21fa926020393d8
Uditha98L <uditha98@yahoo.com>
2026-07-17 17:00:16 -0400

    Create Integration class for WooCommerce
    Add WooCommerce integration class with initialization.

--- SRC/WooCommerce/Integration.php
+++ SRC/WooCommerce/Integration.php
@@ -1,1 +1,53 @@
+<?php
+/**
+ * WooCommerce integration.
+ *
+ * @package KoombiyoShipping
+ */
 
+namespace Koombiyo\Shipping\WooCommerce;
+
+defined( 'ABSPATH' ) || exit;
+
+
+/**
+ * Class Integration
+ */
+class Integration {
+
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+
+		add_action(
+			'woocommerce_init',
+			array(
+				$this,
+				'init',
+			)
+		);
+
+	}
+
+
+	/**
+	 * Initialize integration.
+	 *
+	 * @return void
+	 */
+	public function init(): void {
+
+
+		if ( ! class_exists( 'WooCommerce' ) ) {
+
+			return;
+
+		}
+
+
+	}
+
+
+}
