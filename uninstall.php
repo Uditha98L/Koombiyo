a228651586dda1e4bd992061de922034eb455106
Uditha98L <uditha98@yahoo.com>
2026-07-17 16:32:55 -0400

    Implement uninstall functionality for plugin
    Add uninstall script for Koombiyo Shipping plugin.

--- uninstall.php
+++ uninstall.php
@@ -1,1 +1,26 @@
+<?php
+/**
+ * Uninstall Koombiyo Shipping.
+ *
+ * @package KoombiyoShipping
+ */
 
+defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
+
+
+/*
+ * Remove plugin options.
+ */
+
+delete_option(
+	'koombiyo_shipping_settings'
+);
+
+
+/*
+ * Database cleanup is intentionally disabled.
+ *
+ * Enterprise plugins normally preserve data
+ * unless the user explicitly removes it.
+ *
+ */
