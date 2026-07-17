0cb9f5fa97a5eea3ab37efd6a9ea6d34e1b784c2
Uditha98L <uditha98@yahoo.com>
2026-07-17 16:59:04 -0400

    Add Assets class for admin asset management

--- SRC/Admin/Assets.php
+++ SRC/Admin/Assets.php
@@ -1,1 +1,52 @@
+<?php
+/**
+ * Admin assets.
+ *
+ * @package KoombiyoShipping
+ */
 
+namespace Koombiyo\Shipping\Admin;
+
+defined( 'ABSPATH' ) || exit;
+
+
+/**
+ * Class Assets
+ */
+class Assets {
+
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+
+		add_action(
+			'admin_enqueue_scripts',
+			array(
+				$this,
+				'load',
+			)
+		);
+
+	}
+
+
+	/**
+	 * Load assets.
+	 *
+	 * @return void
+	 */
+	public function load(): void {
+
+
+		wp_enqueue_style(
+			'koombiyo-admin',
+			KOOMBIYO_SHIPPING_URL . 'assets/css/admin.css',
+			array(),
+			KOOMBIYO_SHIPPING_VERSION
+		);
+
+	}
+
+}
